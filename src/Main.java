f390ffd302cbf4c1d736ea99764299a92789b6fb
Ruslan <vip.russle@gmail.com>
2019-08-19 18:45:46 +0300

    Added new function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println(22);
-        System.out.println("Hi!");
+        System.out.println(calculateFootballMatch(1, 3, 1, 3));
     }
+
+    public static int calculateFootballMatch(int scoreOne, int scoreTwo, int betOne, int betTwo)
+    {
+        int result = ((scoreOne == betOne) && (scoreTwo == betTwo)) ? 2 :
+                (scoreOne > scoreTwo && betOne > betTwo) ||
+                ((scoreOne < scoreTwo && betOne < betTwo)) ? 1 : 0;
+        return result;
+    }
+
 }
